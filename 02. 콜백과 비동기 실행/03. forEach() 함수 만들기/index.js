b50f7b48e83a025617a7bb9b563f1775fbeb1787
oneseel <seno10065@gmail.com>
2025-01-05 03:04:20 +0900

    02-03. forEach() 함수 만들기기

---
+++ 02. 콜백과 비동기 실행/03. forEach() 함수 만들기/index.js
@@ -0,0 +1,13 @@
+function forEach(array, callback) {
+  for (let elt of array) {
+    callback(elt);
+  }
+}
+
+const words = ["JavaScript", "Java", "Python"];
+
+// 배열의 요소를 출력하세요. Arrow Function을 사용하세요.
+forEach(words, (word) => console.log(word));
+
+// 배열의 요소를 대문자로 출력하세요. function 키워드로 함수를 선언하고 콜백으로 전달하세요.
+forEach(words, (word) =>  console.log(word.toUpperCase()));
