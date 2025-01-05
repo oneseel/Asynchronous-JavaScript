b385d3b07c36a381120a7325efb5f11edfa2844f
oneseel <seno10065@gmail.com>
2025-01-05 23:42:02 +0900

    02-05. setTimeout() 함수 사용해 보기

---
+++ 02. 콜백과 비동기 실행/05. setTimeout() 함수 사용해 보기/main.js
@@ -0,0 +1,17 @@
+// 모범 답안안
+// function sayHello(name) {
+//   console.log(`Hello ${name}!`);
+// }
+
+// console.log('시작');
+
+// setTimeout(sayHello, 2000, 'Codeit');
+
+// console.log('끝');
+
+// 보너스 실습 모범 답안안
+console.log("시작");
+
+setTimeout((name) => console.log(`Hello ${name}!`), 2000, "Codeit");
+
+console.log("끝");
